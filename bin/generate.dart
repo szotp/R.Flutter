eb9698114aeba3145088d45c74988f964ef1d3be
Paweł Szot <pszot@pgs-soft.com>
2019-12-05 09:47:12 +0100

    use pubspec for arguments

--- bin/generate.dart
+++ bin/generate.dart
@@ -2,12 +2,22 @@
 import 'dart:io';
 
 import 'package:r_flutter/builder.dart';
-import 'package:r_flutter/src/arguments.dart';
 import 'package:r_flutter/src/generator/generator.dart';
+import 'package:yaml/yaml.dart';
 
 main(List<String> args) {
-  var arguments = Arguments();
-  arguments.parse(args);
+  final pubspec = File('pubspec.yaml');
+
+  if (!pubspec.existsSync()) {
+    print('pubspec does not exist');
+    exit(-1);
+  }
+
+  final configRaw = loadYaml(pubspec.readAsStringSync());
+  final arguments = parseYamlArguments(configRaw ?? YamlMap());
+
+  // TODO: should be changeable
+  arguments.outputFilename = 'lib/r.g.dart';
 
   final res = parseResources(arguments);
   final contents = generateFile(res, arguments);
